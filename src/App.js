42dfbf741f4c78d677f196665de4612b83022906
Shahad  Sidek <73951870+shahadsidek@users.noreply.github.com>
2022-11-21 21:25:25 +0300

    added links to spotify and the form created

--- src/App.js
+++ src/App.js
@@ -1,22 +1,56 @@
 import './App.css';
-import { Component } from 'react';
+import { useEffect , useState } from 'react';
+import { axios } from 'axios';
 
-class App extends Component{
-  constructor() {
-    super();
-    this.state = {
-      
+
+
+function App(){
+  const CLIENT_ID = "4c2ef29efafa450786a384f696bbd637"
+  const REDIRECT_URI ="http://localhost:3000"
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+  const RESPONSE_TYPE = "token"
+  const [token,setToken] = useState("") 
+  const [searchKey, setSearchKey] = useState("")
+  
+  useEffect(() => {
+    const hash = window.location.hash
+    let token = window.localStorage.getItem("token")
+
+    if(!token && hash){
+      token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+      window.location.hash=""
+      window.localStorage.setItem("token",token)
+      setToken(token)
     }
+  },[])
+  
+  const logout = () =>{
+    setToken("")
+    window.localStorage.removeItem("token")
   }
 
-  render(){
-    return (
-      <div className="App">
-        <h1> Welcome to the Officially first react website</h1>
-      </div>
-    );
+  const searchArtist = (e) =>{
+    
   }
-}
+
+  return (
+    <div className="App">
+      <h1> Spotify React App</h1>
+      {!token ? 
+      <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}> Login into Spotify</a>
+      : <button onClick={logout}>Logout</button>
+      }
+
+      {token ? 
+        <form onSubmit={searchArtist}>
+          <input type="text" onChange={e=>setSearchKey(e.target.value)}/>
+          <button type="submit">Search</button>
+        </form>  
+        : <h2>Please Log in</h2>
+      }
+  </div>
+  );
+  }
   
 
 export default App;
